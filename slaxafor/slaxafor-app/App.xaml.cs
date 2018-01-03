d4b57d76359f2b3aa3fea5158242ae8d7de1fe8d
jgreitans <juris.greitans@gmail.com>
2018-01-03 23:29:16 +0200

    Add blinking (for testing purposes).

--- slaxafor/slaxafor-app/App.xaml.cs
+++ slaxafor/slaxafor-app/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using LuxaforSharp;
+using slaxafor_app.NotifyIcon;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace slaxafor_app
@@ -13,5 +11,25 @@ namespace slaxafor_app
     /// </summary>
     public partial class App : Application
     {
+        private IDevice _luxafor;
+        private TaskbarIcon _taskbarIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            IDeviceList list = new DeviceList();
+            list.Scan();
+            _luxafor = list.First();
+            _taskbarIcon = (TaskbarIcon)FindResource("NotifyIcon");
+            _taskbarIcon.DataContext = new NotifyIconViewModel(_luxafor);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _taskbarIcon.Dispose();
+            _luxafor.Blink(LedTarget.All, new Color(0, 0, 0), 1);
+            _luxafor.Dispose();
+            base.OnExit(e);
+        }
     }
 }
---
+++ slaxafor/slaxafor-app/NotifyIcon/NotifyIconViewModel.cs
@@ -0,0 +1,57 @@
+﻿using LuxaforSharp;
+using System.Windows;
+
+namespace slaxafor_app.NotifyIcon
+{
+    public class NotifyIconViewModel
+    {
+        private IDevice _luxafor;
+
+        public NotifyIconViewModel(IDevice luxafor)
+        {
+            _luxafor = luxafor;
+        }
+
+        public System.Windows.Input.ICommand ShowWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CanExecuteFunc = () => Application.Current.MainWindow == null,
+                    CommandAction = () =>
+                    {
+                        Application.Current.MainWindow = new MainWindow();
+                        Application.Current.MainWindow.Show();
+                    }
+                };
+            }
+        }
+
+        public System.Windows.Input.ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
+            }
+        }
+
+        public System.Windows.Input.ICommand BlinkCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CommandAction = () => Blink(),
+                    CanExecuteFunc = () => true
+                };
+            }
+        }
+
+        private void Blink()
+        {
+            _luxafor.CarryOutPattern(PatternType.RainbowWave, 1);
+        }
+
+    }
+}
